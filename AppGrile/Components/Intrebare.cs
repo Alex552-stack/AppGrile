9fad912c333fe97e18ea16b8223662ce1c64904b
Alex <Alex@ALEXSLITTLEWARM>
2023-12-06 08:50:16 +0200

    update

--- AppGrile/Components/Intrebare.cs
+++ AppGrile/Components/Intrebare.cs
@@ -8,58 +8,58 @@ namespace AppGrile.Components
 {
 	public partial class Intrebare : UserControl
 	{
-		DateIntrebare Date;
-		public event EventHandler MyCustomAction;
-		CheckBox[] Vector;
-		bool ok;
+		DateIntrebare Date; //datele intrebarii
+		public event EventHandler MyCustomAction; //trigger pentru event in clasa parinte
+		CheckBox[] Vector; 
+		bool ok; //intrebare raspunsa corect/incorect
 		public Intrebare(DateIntrebare date)
 		{
 			Date = date;
 			InitializeComponent();
-			Vector = new CheckBox[date.Optiuni.Count()];
+			Vector = new CheckBox[date.Optiuni.Count()]; // vector de marimea optiunilor(4)
 			Initializare();
 
 		}
 
 		private void Initializare()
 		{
-			NumeIntrebare.Text = Date.textIntrebare;
-			TabelOptiuniIntrebare.AutoSize = false;
-			TabelOptiuniIntrebare.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+			NumeIntrebare.Text = Date.textIntrebare; //intrebarea in sine
+			TabelOptiuniIntrebare.AutoSize = false; //autosize
+			TabelOptiuniIntrebare.RowStyles.Add(new RowStyle(SizeType.AutoSize)); //autosize
 			for (int j = 0; j < Date.Optiuni.Count(); j++)
 			{
-				TabelOptiuniIntrebare.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+				TabelOptiuniIntrebare.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize)); //autosize
 
-				CheckBox checkBox = new CheckBox();
-				checkBox.Text = Date.Optiuni[j];
-				checkBox.AutoSize = true;
-				TabelOptiuniIntrebare.Controls.Add(checkBox, j, 0);
-				Vector[j] = checkBox;
+				CheckBox checkBox = new CheckBox(); //checkBox element
+				checkBox.Text = Date.Optiuni[j]; //text-ul optiunilor
+				checkBox.AutoSize = true; //autosize
+				TabelOptiuniIntrebare.Controls.Add(checkBox, j, 0); //adaugat checkBox ul in grafica
+				Vector[j] = checkBox; //salvat checkBox-ul
 			}
 			if(Date.Poza != "")
 			{
-				ContainerPoza.ImageLocation = $"../../Images/{Date.Poza}";
+				ContainerPoza.ImageLocation = $"../../Grila/{Date.Poza}"; //path poza
 			}
 
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			ok = true;
+			ok = true; //incep de la ideea ca intrebarea este raspunsa corect
 			for (int i = 0; i < Date.Optiuni.Count(); i++)
 			{
-				if (Vector[i].Checked != Date.raspunsuriCorecte[i])
+				if (Vector[i].Checked != Date.raspunsuriCorecte[i]) //caut situatie in care intrebarea este raspunsa incorect
 				{
 					ok = false;
 					break;
 				}
 			}
-			if (ok)
+			if (ok) //daca este corect, culoarea butonului verde
 				button1.BackColor = Color.Green;
-			else
+			else //daca este gresita, culoarea butonului rosu
 				button1.BackColor = Color.Red;
-			button1.Enabled = false;
-			MyCustomAction.Invoke(ok, EventArgs.Empty);
+			button1.Enabled = false; //acum pot sa trec la urmatoarea intrebare
+			MyCustomAction.Invoke(ok, EventArgs.Empty); //folosit event-ul din clasa parinte 
 		}
 	}
 }
--- AppGrile/Form1.cs
+++ AppGrile/Form1.cs
@@ -22,31 +22,31 @@ namespace AppGrile
 			InitializeComponent();
 			CitireFisier();
 			UpdateEcran();
-			textIntrebariCorecte.Text = NrIntrebariCorecte.ToString();
-			textIntrebariGresite.Text = NrIntrebariGresite.ToString();
-			textIntrebariRamase.Text = (NrIntrebari - IntrebareCurenta - 1).ToString();
+			textIntrebariCorecte.Text = NrIntrebariCorecte.ToString(); //init text
+			textIntrebariGresite.Text = NrIntrebariGresite.ToString(); //init text
+			textIntrebariRamase.Text = (NrIntrebari - IntrebareCurenta - 1).ToString(); //init text
 		}
 		private void UpdateEcran()
 		{
-			if (IntrebareCurenta + 1 < dateIntrebare.Count)
+			if (IntrebareCurenta + 1 < dateIntrebare.Count) //atata timp cat nu s-a ajuns la sfarsit
 			{
-				ContainerIntrebare.Controls.Clear();
-				NextButton.Enabled = false;
-				IntrebareCurenta++;
-				Intrebare intrebare = new Intrebare(dateIntrebare[IntrebareCurenta]);
-				intrebare.Text = "Custom Element";
-				intrebare.MyCustomAction += Intrebare_MyActionOccurred;
-				ContainerIntrebare.Controls.Add(intrebare);
+				ContainerIntrebare.Controls.Clear(); //sterge intrebarea curenta
+				NextButton.Enabled = false; //disable la butonul de mers mai departe
+				IntrebareCurenta++; 
+				Intrebare intrebare = new Intrebare(dateIntrebare[IntrebareCurenta]); //creare obiect now de tip Intrebare
+				intrebare.Text = "Custom Element"; 
+				intrebare.MyCustomAction += Intrebare_MyActionOccurred; //trigger ul de event(imi permite sa accesez o functie din acest obiect din clasa Intrebare)
+				ContainerIntrebare.Controls.Add(intrebare); //adaugarea intrebarii pe ecran
 			}
-			else
+			else //daca s-a ajuns la final
 			{
-				ContainerIntrebare.Controls.Clear();
-				ContainerIntrebare.Controls.Add(new EcranFinal());
-				NextButton.Enabled = false;
+				ContainerIntrebare.Controls.Clear(); //sterge intrebarea
+				ContainerIntrebare.Controls.Add(new EcranFinal()); //adauga ecranul final
+				NextButton.Enabled = false; 
 			}
 		}
 
-		private void Intrebare_MyActionOccurred(object sender, EventArgs e)
+		private void Intrebare_MyActionOccurred(object sender, EventArgs e) //evenimentul care e apelat din intrebare
 		{
 			bool ok = (bool)sender;
 			if (ok)
@@ -61,30 +61,30 @@ namespace AppGrile
 
 		private void CitireFisier()
 		{
-			string filePath = @"../../input.txt";
-			using (StreamReader sr = new StreamReader(filePath))
+			string filePath = @"../../Grila/test1.txt";
+			using (StreamReader sr = new StreamReader(filePath))//citire fisier
 			{
-				NrIntrebari = int.Parse(sr.ReadLine());
-				IntrebareCurenta = -1;
-				NrIntrebariCorecte = 0;
+				NrIntrebari = int.Parse(sr.ReadLine()); //prima linie nr de intrebare
+				IntrebareCurenta = -1;	//nu este nicio intrebare incarcata momentan
+				NrIntrebariCorecte = 0; 
 				NrIntrebariGresite = 0;
-				sr.ReadLine();
+				sr.ReadLine(); //spatiu gol
 
-				while (!sr.EndOfStream)
+				while (!sr.EndOfStream)//pana nu se termina fisierul
 				{
 					DateIntrebare intrebare = new DateIntrebare();
-					intrebare.textIntrebare = sr.ReadLine();
-					//intrebare.Optiuni = new List<string>();
+					intrebare.textIntrebare = sr.ReadLine(); //citire textIntrebare
+					
 					for (int i = 0; i < 4; i++)
 					{
-						intrebare.Optiuni[i] = sr.ReadLine();
+						intrebare.Optiuni[i] = sr.ReadLine(); //optiunile
 					}
 					for (int i = 0; i < 4; i++)
 					{
-						intrebare.raspunsuriCorecte[i] = sr.ReadLine().Contains("1");
+						intrebare.raspunsuriCorecte[i] = sr.ReadLine().Contains("1"); //true/false, folosesc contains pentru eventualele caractere in plus cum ar fi ' '
 					}
-					intrebare.Poza = sr.ReadLine();
-					dateIntrebare.Add(intrebare);
+					intrebare.Poza = sr.ReadLine(); //nume fisier intrebare
+					dateIntrebare.Add(intrebare); 
 					sr.ReadLine();
 				}
 			}
